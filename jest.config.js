7bcd642cfa974ac72f48b1616e8fc720a10cacb5
Rafael "Brodinho" Guimarães <465634+obrodinho@users.noreply.github.com>
2023-01-02 23:38:52 -0300

    chore: updates jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    roots: [
+        '<rootDir>/src',
+        '<rootDir>/tests'
+    ],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/main/**'
+    ],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '@/tests/(.*)': '<rootDir>/tests/$1',
+        '@/(.*)': '<rootDir>/src/$1'
+    }
+}
