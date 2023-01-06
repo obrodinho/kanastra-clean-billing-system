8f3cbb4f18157ef531bd4f5ec1367649b511dd99
Rafael "Brodinho" Guimarães <465634+obrodinho@users.noreply.github.com>
2023-01-06 02:55:00 -0300

    chore: adds jest config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    roots: [
-        '<rootDir>/src',
-        '<rootDir>/tests'
-    ],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.ts',
-        '!<rootDir>/src/main/**'
-    ],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    testEnvironment: 'node',
-    transform: {
-        '.+\\.ts$': 'ts-jest'
-    },
-    moduleNameMapper: {
-        '@/tests/(.*)': '<rootDir>/tests/$1',
-        '@/(.*)': '<rootDir>/src/$1'
-    }
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 }
