cebcb8347ef6aba3657263d5c0df00d73d93b194
Rafael "Brodinho" Guimarães <465634+obrodinho@users.noreply.github.com>
2023-01-06 19:00:05 -0300

    feat: concrete mongo repository for debt additions

--- src/infra/db/mongodb/debt-mongo-repository.ts
+++ src/infra/db/mongodb/debt-mongo-repository.ts
@@ -1,16 +1,23 @@
-import { MongoHelper } from '@/infra/db'
-import { AddManyDebtRepository } from '@/data/protocols/db'
+import {MongoHelper} from '@/infra/db'
+import {AddManyDebtRepository} from '@/data/protocols/db'
+import {Document, MongoServerError} from 'mongodb'
 
 export class DebtMongoRepository implements AddManyDebtRepository {
   private readonly collection = 'debts'
 
-  async addMany (debts: AddManyDebtRepository.Params[]): Promise<AddManyDebtRepository.Result> {
-    const debtsCollection = MongoHelper.getCollection(this.collection)
-    const result = await debtsCollection.insertMany(debts)
-    const { insertedIds } = result
+  async addMany(debts: AddManyDebtRepository.Params[]): Promise<AddManyDebtRepository.Result> {
     const insertedDebtIds = []
-    for (const [insertedId] of new Map(Object.entries(insertedIds))) {
-      insertedDebtIds.push(insertedId.toString())
+    const debtsCollection = MongoHelper.getCollection(this.collection)
+    for (const debt of debts) {
+      const doc: Document = {...debt, _id: debt.debtId}
+      try {
+        const insertOneResult = await debtsCollection.insertOne(doc)
+        insertedDebtIds.push(insertOneResult.insertedId)
+      } catch (e) {
+        if (e instanceof MongoServerError && e.code === 11000) {
+          continue
+        }
+      }
     }
 
     return insertedDebtIds
